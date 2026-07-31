3d43f18fb006ad493f825053f9e90ab6efb195d0
fagbegnondimitri-ops <fagbegnondimitri@gmail.com>
2026-07-31 20:51:23 +0100

    Delete exo 3.py

--- exo 3.py
+++
@@ -1,7 +0,0 @@
-notes = [12, 8, 15, 17, 9]
-total=0
-for i in range(len(notes)):
-    print(f"Note {i+1} = {notes[i]}")
-    total += notes[i]
-print("somme =",total)
-print("moyenne=",total/len(notes))
