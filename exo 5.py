b267defcc757fb2d9f60335a97cc149465ec0eec
fagbegnondimitri-ops <fagbegnondimitri@gmail.com>
2026-07-31 20:51:48 +0100

    Delete exo 5.py

--- exo 5.py
+++
@@ -1,5 +0,0 @@
-personne={"nom":"Marie","age":25,"ville":"lyon"}
-print(f"{personne['nom']} a {personne['age']} ans et vit a {personne['ville']}")
-personne["age"]= 26
-personne["email"]="fagbegnondimitri@gmail.com" 
-print (personne.keys())
